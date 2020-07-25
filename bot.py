b07d6cbe432fe7afe7cb135b701cb2f297695f8e
Amir Bazanov <flash5651311@gmail.com>
2020-07-25 16:07:31 +0500

    Add webhook

--- bot.py
+++ bot.py
@@ -3,15 +3,22 @@ from telebot import types
 from telebot.types import Message
 import covid
 from translate import Translator
-import requests
+from flask import Flask, request
+from flask_sslify import SSLify
+import os
 
-TOKEN = 'TOKEN'
+
+server = Flask(__name__)
+sslify = SSLify(server)
 translate = Translator(to_lang='en', from_lang='ru')
 covid = covid.Covid()
+TOKEN = '918361897:AAFaWodgWzVYMnAUZNLP6O4g8YBzs2rbhNA'
 bot = telebot.TeleBot(TOKEN)
+
 alias = {'worldwide': 'world', 'usa': 'us', 'america': 'us',
          'england': 'united kingdom', 'check republic': 'czechia',
-         'africa': 'central african republic', 'arab emirates': 'united arab emirates'}
+         'africa': 'central african republic', 'arab emirates': 'united arab emirates', 'usd': 'us',
+         "united states": 'us'}
 all_country = [i['name'].lower() for i in covid.list_countries()]
 
 
@@ -19,11 +26,11 @@ all_country = [i['name'].lower() for i in covid.list_countries()]
 def start(message: Message):
 	markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
 	btn1 = types.KeyboardButton('Во всём мире🌎')
-	btn2 = types.KeyboardButton('Украина🇺🇦')
+	btn2 = types.KeyboardButton('США🇺🇸')
 	btn3 = types.KeyboardButton('Россия🇷🇺')
 	btn4 = types.KeyboardButton('Узбекистан🇺🇿')
 	markup.add(btn1, btn2, btn3, btn4)
-	
+
 	send_message = f"<b>Привет {message.from_user.first_name}!</b>\n" \
 	               f"Чтобы узнать данные про коронавирус напишите\n" \
 	               f"название страны, например: США, Украина, Россия и так далее\n" \
@@ -38,38 +45,39 @@ def start(message: Message):
 def sendInfo(message: Message):
 	def_name = str(message.text).title()
 	country = translate.translate(def_name).lower()
-	if '🌎' in def_name or '🇺🇦' in def_name or '🇷🇺' in def_name or '🇺🇿' in def_name:
+	if '🌎' in def_name or "🇺🇸" in def_name or '🇷🇺' in def_name or '🇺🇿' in def_name:
 		def_name = def_name[:-2]
 		country = translate.translate(def_name).lower()
+		print(country)
 	if country in alias.keys():
 		country = alias[country]
 	if country[0] == def_name[0].lower() and len(def_name) < len(country):
 		country = country[len(def_name) + 2:-1]
 	try:
-		get_population = requests.get(f'https://coronavirus-tracker-api.herokuapp.com/v2/locations?country={country}&source=jhu')
-		population = get_population.json()['locations'][0]['country_population']
+		if country in all_country:
+			print('Searched: ' + country + " User " + str(message.from_user.first_name) + " " + str(
+				message.from_user.last_name))
 		send_stat = covid.get_status_by_country_name(country)
 		bot.send_message(message.chat.id, f"<b><u>🇲🇶{def_name}🇲🇶:</u></b>\n"
-		                                  f"<i>🧍Население🧍: </i>{population}\n"
 		                                  f"<i>🦠Больных🦠: </i>{send_stat['confirmed']}\n"
 		                                  f"<i>💀Умерших💀: </i>{send_stat['deaths']}\n"
 		                                  f"<i>💉Выздоровевших💉: </i>{send_stat['recovered']}", parse_mode='html')
-		print('Searched: ' + country + " User " + message.from_user.first_name + " " + message.from_user.last_name)
-	except KeyError or ValueError:
+	except ValueError or KeyError:
 		if country == 'world':
 			bot.send_message(message.chat.id, f'<u><b>🗺️В мире🗺️:</b></u>\n'
-			                                  f"<i>🧍Население🧍: </i>7383008820\n"
 			                                  f'<i>🦠Больных🦠: </i>{covid.get_total_confirmed_cases()}\n'
 			                                  f'<i>💀Умерших💀: </i>{covid.get_total_deaths()}\n'
 			                                  f'<i>💉Выздоровевших💉: </i>{covid.get_total_recovered()}',
 			                 parse_mode='html')
 		else:
-			print('Error name ' + country + ' User Name ' + message.from_user.first_name + " " + message.from_user.last_name)
+			print(
+				'Error name ' + country + ' User Name ' + str(message.from_user.first_name) + " " + str(
+					message.from_user.last_name))
 			bot.send_message(message.chat.id, f"<b>❌Страны {def_name} не существует❌</b>",
 			                 parse_mode='html')
 
 
-@bot.inline_handler(lambda query: True)
+@bot.inline_handler(lambda query: 10 > 0)
 def query_text(query: types.InlineQuery):
 	translated = translate.translate(query.query).lower()
 	if translated in alias.keys():
@@ -77,40 +85,42 @@ def query_text(query: types.InlineQuery):
 	if translated in all_country:
 		send_stat = covid.get_status_by_country_name(translated)
 	else:
-		send_stat = None
+		send_stat = {'confirmed': "wrong"}
 	try:
-		get_population = requests.get(
-			f'https://coronavirus-tracker-api.herokuapp.com/v2/locations?country={translated}&source=jhu')
-		population = get_population.json()['locations'][0]['country_population']
 		world = types.InlineQueryResultArticle('1', "Мир", types.InputTextMessageContent(
 			f'<u><b>🗺️В мире🗺️:</b></u>\n'
-			f"<i>🧍Население🧍: </i>7383008820\n"
 			f'<i>🦠Больных🦠: </i>{covid.get_total_confirmed_cases()}\n'
 			f'<i>💀Умерших💀: </i>{covid.get_total_deaths()}\n'
 			f'<i>💉Выздоровевших💉: </i>{covid.get_total_recovered()}',
 			parse_mode='html'))
 		country = types.InlineQueryResultArticle('2', query.query.title(), types.InputTextMessageContent(
 			f"<b><u>🇲🇶{query.query}🇲🇶:</u></b>\n"
-			f"<i>🧍Население🧍: </i>{population}\n"
 			f"<i>🦠Больных🦠: </i>{send_stat['confirmed']}\n"
 			f"<i>💀Умерших💀: </i>{send_stat['deaths']}\n"
-			f"<i>💉Выздоровевших💉: </i>{send_stat['recovered']}", parse_mode='html'))
-		bot.answer_inline_query(query.id, [world, country])
-	except Exception or KeyError or ValueError as e:
-		bot.answer_inline_query(query.id, [
-				types.InlineQueryResultArticle('1', "Мир",
-				                               types.InputTextMessageContent(
-					                               f'<u><b>🗺️В мире🗺️:</b></u>\n'
-					                               f"<i>🧍Население🧍: </i>7383008820\n"
-					                               f'<i>🦠Больных🦠: </i>{covid.get_total_confirmed_cases()}\n '
-					                               f'<i>💀Умерших💀: </i>{covid.get_total_deaths()}\n'
-					                               f'<i>💉Выздоровевших💉: </i>{covid.get_total_recovered()}',
-					                               parse_mode='html')),
-				types.InlineQueryResultArticle('2', f'{query.query} странны нету',
-				                               types.InputTextMessageContent(
-					                               f'<b>Cтраны {query.query} нету</b>', parse_mode='html'))])
-		
-		print("Inline Error: " + str(e))
+			f"<i>💉Выздоровевших💉: </i>{send_stat['recovered']}"
+			if send_stat['confirmed'] != 'wrong' else f"<b>❌Такой страны не существует❌</b>", parse_mode='html'))
+		bot.answer_inline_query(query.id, [world, country if send_stat['confirmed'] != 'wrong'
+		else types.InlineQueryResultArticle('2', f'{query.query} странны нету',
+		                                    types.InputTextMessageContent(f'<b>Cтраны {query.query} нету</b>',
+		                                                                  parse_mode='html'))])
+	except Exception as e:
+		print(str(e) + " Inline Error ")
 
 
-bot.polling(none_stop=True, timeout=60)
+@server.route("/" + TOKEN, methods=["POST"])
+def getMessage():
+	for i in [telebot.types.Update.de_json(request.get_json())]:
+		print(i)
+	bot.process_new_updates([telebot.types.Update.de_json(request.get_json())])
+	return "!", 200
+
+
+@server.route("/")
+def web_hook():
+	bot.remove_webhook()
+	bot.set_webhook(url='https://8e5ba1174a2e.ngrok.io/' + TOKEN)
+	return "!", 200
+
+
+if __name__ == "__main__":
+	server.run(host="localhost", port=int(os.environ.get("PORT", 5000)))
